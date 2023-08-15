b503cef4ab9d41d3a073784d7a1c3240d3c2844c
Arthur Speroto Cantarela <ra135017@snake.lab.ic.unicamp.br>
2023-08-14 22:43:50 -0300

    Criando rotina principal

--- MC322/src/Main.java
+++ MC322/src/Main.java
@@ -1,8 +1,42 @@
+import actors.Member;
+import actors.Employee;
+import resources.Book;
+import flows.Loan;
+import java.util.Date;
 
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Book book = new Book(
+		    "Guia do Mochilheiro das Galáxias",
+		    "Douglas Adams",
+		    1,
+		    "Scifi"
+		);
+		Member member = new Member(
+			"Arthur Cantarela",
+			135017
+		);
+		Employee employee = new Employee(
+			"Gabriel Haj",
+			171859
+		);
+		
+		Loan loan = new Loan(
+		    book,
+		    member,
+		    employee,
+		    new Date()
+		);
+		
+		System.out.println("Que livro foi emprestado? " + loan.getBook().getTitle());
+		System.out.println("Quem pegou emprestado? " + loan.getMember().getName());
+		System.out.println("Que funcionário emprestou? " + loan.getEmployee().getName());
+		
+		System.out.println("Foi devolvido? " + (loan.isReturned() ? "Sim" : "Não"));
+		
+		System.out.println("Devolvendo...");
+		loan.returnBook();
+		
+		System.out.println("Foi devolvido? " + (loan.isReturned() ? "Sim" : "Não"));
 	}
-
 }
