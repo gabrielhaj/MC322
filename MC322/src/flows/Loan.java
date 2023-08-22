6eedf9ba16999afcb6ade9f17c060d3bc56ce3e3
Arthur Speroto Cantarela <ra135017@titas.lab.ic.unicamp.br>
2023-08-21 22:37:48 -0300

    new classes and its relations

--- MC322/src/flows/Loan.java
+++ MC322/src/flows/Loan.java
@@ -1,21 +1,15 @@
 package flows;
 
-import actors.Employee;
-import actors.Member;
 import resources.Book;
 import java.util.Date;
 
 public class Loan {
 	private Book book;
-	private Member member;
-	private Employee employee;
 	private Date dueDate;
 	private Date returnedAt;
 	
-	public Loan(Book book, Member member, Employee employee, Date dueDate) {
+	public Loan(Book book, Date dueDate) {
 		this.book = book;
-		this.member = member;
-		this.employee = employee;
 		this.dueDate = dueDate;
 		returnedAt = null;
 	}
@@ -23,14 +17,6 @@ public class Loan {
 	// getter/setter
 	public Book getBook() {
 		return book;
-	}
-
-	public Member getMember() {
-		return member;
-	}
-	
-	public Employee getEmployee() {
-		return employee;
 	}
 
 	public Date getDueDate() {
@@ -44,5 +30,9 @@ public class Loan {
 	
 	public void returnBook() {
 		this.returnedAt = new Date();
+	}
+	
+	public void renovate(Date newDate) {
+		dueDate = newDate;
 	}
 }
---
+++ MC322/src/flows/Reservation.java
@@ -0,0 +1,5 @@
+package flows;
+
+public class Reservation {
+
+}
